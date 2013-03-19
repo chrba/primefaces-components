a5a53239cfcb4fb33cf915820f057da63d69df18
Christian Bannes <cbannes@gmail.com>
2013-03-19 18:54:11 +0100

    initial commit

---
+++ src/main/java/de/morten/sample/servlet/Servlet.java
@@ -0,0 +1,22 @@
+package de.morten.sample.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name="myServlet", urlPatterns={"/servlet"})
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(final HttpServletRequest req, final HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/plain");
+		PrintWriter writer = res.getWriter();
+		writer.write("Hello, Servlet");
+	}	
+}
