7af44c4f6dd2fffd1e0e5868b252cd5839a59388
Christian Bannes <cbannes@gmail.com>
2013-04-06 23:23:57 +0200

    implmenting composite component "note"

--- src/main/java/de/morten/web/FileUploadController.java
+++ src/main/java/de/morten/web/FileUploadController.java
@@ -32,7 +32,6 @@ public class FileUploadController {
 	
     public void upload(FileUploadEvent event) {
     	try {
-    		System.out.println("helo");
 			final UploadedFile file = event.getFile();
 			final BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputstream()));
 			
